a6c1473b27c5a7e77cab265d42b950736d5a30a3
anismynm <ddoe0318@naver.com>
2024-11-07 17:44:42 +0900

    feat: 최초 등록

---
+++ Backend/IdolBom/src/main/java/Fav_I/IdolBom/IdolBomApplication.java
@@ -0,0 +1,13 @@
+package Fav_I.IdolBom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdolBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdolBomApplication.class, args);
+	}
+
+}
---
+++ Backend/IdolBom/src/test/java/Fav_I/IdolBom/IdolBomApplicationTests.java
@@ -0,0 +1,13 @@
+package Fav_I.IdolBom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdolBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

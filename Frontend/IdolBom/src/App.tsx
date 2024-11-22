9720b88e4e59c9593f98ea81aa2a84c14d93b3a7
milevol <sarang052580@gmail.com>
2024-11-23 08:42:18 +0900

    feat: add Ticketing

--- Frontend/IdolBom/src/App.tsx
+++ Frontend/IdolBom/src/App.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './screens/HomeScreen';
-import BottomBar from './components/common/BottomBar';
 import TopBar from './components/common/TopBar';
+import BottomBar from './components/common/BottomBar';
+import HomeScreen from './screens/HomeScreen';
 import ScheduleDetailScreen from './screens/ScheduleDetailScreen';
+import FindTicketAgentScreen from './screens/ticketing/FindTicketAgentScreen';
+import MatchTicketAgentScreen from './screens/ticketing/MatchTicketAgentScreen';
+import TicketScreen from './screens/ticketing/TicketScreen';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -17,6 +20,7 @@ const MainTabs = () => {
       <TopBar />
       <Tab.Navigator tabBar={() => <BottomBar />}>
         <Tab.Screen name="Home" component={HomeStack} options={{ headerShown: false }} />
+        <Tab.Screen name="Tickets" component={TicketStack} options={{ headerShown: false }} />
       </Tab.Navigator>
     </>
   );
@@ -27,6 +31,16 @@ const HomeStack = () => {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="ScheduleDetail" component={ScheduleDetailScreen} />
+      <Stack.Screen name="FindTicketAgent" component={FindTicketAgentScreen} />
+      <Stack.Screen name="MatchTicketAgent" component={MatchTicketAgentScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const TicketStack = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="TicketScreen" component={TicketScreen} />
     </Stack.Navigator>
   );
 };

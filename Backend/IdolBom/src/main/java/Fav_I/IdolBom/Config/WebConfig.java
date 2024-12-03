c93efd72f854291c974c9873a6f81ef00dfa34be
anismynm <ddoe0318@naver.com>
2024-12-03 19:03:43 +0900

    fix: 배포 관련 설정

--- Backend/IdolBom/src/main/java/Fav_I/IdolBom/Config/WebConfig.java
+++ Backend/IdolBom/src/main/java/Fav_I/IdolBom/Config/WebConfig.java
@@ -10,11 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://localhost:8081")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Authorization", "Content-Type")
-                .exposedHeaders("Custom-Header")
-                .allowCredentials(true);
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(false);
     }
 }
 
